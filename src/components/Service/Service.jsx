ad1ad9e0d6fd8800e6edac36448dade2800a9808
IT genius <94828095+nikita-reshetnyak@users.noreply.github.com>
2022-07-23 00:17:06 +0300

    add price block

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -15,8 +15,17 @@ export const Service = ({ refs }) => {
                         <div className="service-section__text-block text-block ">
                             <p> Выезды на погружения совершаем в приятной дружеской атмосфере, без суеты и спешки. Вместе с нашими дайверами выбираем место погружения в зависимости от подготовки и опыта дайверов.
                                 На выбор дайверам предоставляется множество погружений разного уровня. Каждый день мы делим дайверов на небольшие группы и отправляемся в самые красивые места на дно Черного моря, куда совершаем погружения как с берега, так и с лодки.
-                              
+
                             </p>
+                        </div>
+                        <div className="service-section__price-block">
+                            <div className="service-section__price-block-item">
+                                <span>1900</span> руб - 1,5 часa
+                            </div>
+                            <div className="service-section__price-block-item">
+                                <span>600 </span>руб - фото и видео
+                            </div>
+
                         </div>
 
                     </div>
